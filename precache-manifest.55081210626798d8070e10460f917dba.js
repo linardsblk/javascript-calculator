bad681463acca1169e57fad58244c27cf73938b1
linardsblk <linards.bulks@gmail.com>
2020-05-18 21:24:22 +0300

    Updates

---
+++ precache-manifest.55081210626798d8070e10460f917dba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d922f713fa7a4b9857ed0b32ebac00c1",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "6085d444c9554702a4bc",
+    "url": "/javascript-calculator/static/css/main.3cd30ec6.chunk.css"
+  },
+  {
+    "revision": "065e21f5670231cb0a41",
+    "url": "/javascript-calculator/static/js/2.a2c123d2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/javascript-calculator/static/js/2.a2c123d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6085d444c9554702a4bc",
+    "url": "/javascript-calculator/static/js/main.99bd4f44.chunk.js"
+  },
+  {
+    "revision": "7f7a07dc54bd4ee72e66",
+    "url": "/javascript-calculator/static/js/runtime-main.344c365a.js"
+  }
+]);
